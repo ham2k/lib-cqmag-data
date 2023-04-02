d57a03836f4e43f06034fa8ab3f413cb127da165
Sebastian Delmont <sd@notso.net>
2023-04-01 22:08:23 -0400

    export CQWW entities

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const CQZONES = require('../data/cqzones.json')
 const CQZONES_FOR_STATES = require('../data/cqz-for-states.json')
+const CQWW_ENTITIES = require("./data/cqwwByPrefix.json")
 
 module.exports = {
   CQZONES,
-  CQZONES_FOR_STATES
+  CQZONES_FOR_STATES,
+  CQWW_ENTITIES
 }
